5746bf1f7111d864987042b10f1d819b830c4c3e
Anderson de Oliveira <andoliv@gmail.com>
2021-04-12 23:00:28 -0300

    Removed service.

--- frontend/src/app/photo-approval/photo-approval.component.ts
+++ frontend/src/app/photo-approval/photo-approval.component.ts
@@ -2,7 +2,8 @@ import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute, Router} from "@angular/router";
 import {HttpClient} from "@angular/common/http";
 import {DomSanitizer} from "@angular/platform-browser";
-import {PhotoService} from "../services/photo.service";
+import {PhotoData} from "../photo";
+import {interval, Subscription} from "rxjs";
 
 @Component({
   selector: 'app-photo-approval',
@@ -14,27 +15,31 @@ export class PhotoApprovalComponent implements OnInit {
   public id: string;
   public photoSrc: any;
   private apiUrl: string;
+  private updateSubscription: Subscription;
 
   constructor(private route: ActivatedRoute, private http: HttpClient,
-              private router: Router, private sanitizer: DomSanitizer,
-              private service: PhotoService) {
+              private router: Router, private sanitizer: DomSanitizer) {
     this.apiUrl = "http://localhost:8080/photo-service/api/photos/";
   }
 
-  ngOnInit(): void {
+  public ngOnInit() {
     this.id = this.route.snapshot.paramMap.get("id");
     this.apiUrl = this.apiUrl.concat(this.id);
+    console.log(this.apiUrl);
 
-    this.service.getPhotoById(this.id).subscribe(response => {
-      this.photoSrc = this.sanitizer.bypassSecurityTrustUrl('data:image/png;base64,' + response.data);
-    }, responseError => {
-      this.router.navigate(['**']);
-      console.error('There was an error!', responseError);
-    });
+    this.http.get(this.apiUrl)
+      .subscribe((response: PhotoData) => {
+        console.log("this.photoSrc before: " + this.photoSrc);
+        this.photoSrc = this.sanitizer.bypassSecurityTrustResourceUrl(`data:image/png;base64,${response.data}`);
+        console.log("this.photoSrc after: " + this.photoSrc);
+      }, responseError => {
+        this.router.navigate(['**']);
+        console.error('There was an error!', responseError);
+      });
   }
 
   public ngAfterViewInit() {
-
+    console.log("this.photoSrc ngAfterViewInit: " + this.photoSrc);
   }
 
   ngOnDestroy() {
